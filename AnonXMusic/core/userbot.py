c35f3d28a4491e5125302352bebbb5958423536c
cutanobi <167220489+cutanobi@users.noreply.github.com>
2024-05-20 22:45:22 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/NOBITA_KI_DUNIYA1")
-                await self.one.join_chat("https://t.me/+oyXOWKIKPIpiYTZl")
+                await self.one.join_chat("https://t.me/ll_BAIRAGI_DP_ll")
+                await self.one.join_chat("https://t.me/+FSkhK9hV1TtkNTk9")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("https://t.me/NOBITA_KI_DUNIYA1")
-                await self.two.join_chat("https://t.me/+oyXOWKIKPIpiYTZl")
+                await self.two.join_chat("https://t.me/ll_BAIRAGI_DP_ll")
+                await self.two.join_chat("https://t.me/+FSkhK9hV1TtkNTk9")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("https://t.me/NOBITA_KI_DUNIYA1")
-                await self.three.join_chat("https://t.me/+oyXOWKIKPIpiYTZl")
+                await self.three.join_chat("https://t.me/ll_BAIRAGI_DP_ll")
+                await self.three.join_chat("https://t.me/+FSkhK9hV1TtkNTk9")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("https://t.me/NOBITA_KI_DUNIYA1")
-                await self.four.join_chat("https://t.me/+oyXOWKIKPIpiYTZl")
+                await self.four.join_chat("https://t.me/ll_BAIRAGI_DP_ll")
+                await self.four.join_chat("https://t.me/+FSkhK9hV1TtkNTk9")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("https://t.me/NOBITA_KI_DUNIYA1")
-                await self.five.join_chat("https://t.me/+oyXOWKIKPIpiYTZl")
+                await self.five.join_chat("https://t.me/ll_BAIRAGI_DP_ll")
+                await self.five.join_chat("https://t.me/+FSkhK9hV1TtkNTk9")
             except:
                 pass
             assistants.append(5)
